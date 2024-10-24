6bf15e6396f4487807097cabf881c1e1ee39feeb
Facunfer <157515744+Facunfer@users.noreply.github.com>
2024-10-24 01:48:23 -0300

    Add files via upload

--- mapa.py
+++ mapa.py
@@ -367,122 +367,123 @@ st.image(logo_url, use_column_width='auto', output_format='PNG', width=100)
 # Crear un selector de pestañas
 tabs = st.selectbox("Seleccione una pestaña", ["Circuitos", "Comunas", "Ballotage"])
 
+tabs = st.selectbox("Seleccione una pestaña", ["Circuitos", "Comunas", "Ballotage"])
+
 if tabs == "Circuitos":
     st.subheader("Circuitos")
 
-comunas_seleccionadas = st.multiselect('Selecciona Comunas', ['Todas las Comunas'] + resultados2['comuna_id'].unique().tolist())
+    comunas_seleccionadas = st.multiselect('Selecciona Comunas', ['Todas las Comunas'] + resultados2['comuna_id'].unique().tolist())
 
-# Selector de tipo de capa
-tipo_capa = st.selectbox('Selecciona el tipo de capa', ['Votos por Circuito', 'Porcentaje por Circuito'])
+    # Selector de tipo de capa
+    tipo_capa = st.selectbox('Selecciona el tipo de capa', ['Votos por Circuito', 'Porcentaje por Circuito'])
 
-if 'Todas las Comunas' in comunas_seleccionadas:
-    resultados_filtrados = resultados2
-else:
-    resultados_filtrados = resultados2[resultados2['comuna_id'].isin(comunas_seleccionadas)]
+    if 'Todas las Comunas' in comunas_seleccionadas:
+        resultados_filtrados = resultados2
+    else:
+        resultados_filtrados = resultados2[resultados2['comuna_id'].isin(comunas_seleccionadas)]
 
-# Determinar la columna de color según el tipo de capa
-if 'Votos' in tipo_capa:
-    color_column = 'llavotos'
-    legend_name = 'Votos de LLA por Comuna'
-else:
-    color_column = '%LLA'
-    legend_name = 'Porcentaje de Votos LLA'
+    # Determinar la columna de color según el tipo de capa
+    if 'Votos' in tipo_capa:
+        color_column = 'llavotos'
+        legend_name = 'Votos de LLA por Comuna'
+    else:
+        color_column = '%LLA'
+        legend_name = 'Porcentaje de Votos LLA'
 
-color_continuous_scale = [
-    (0.0, "red"),
-    (0.25, "orange"),
-    (0.5, "yellow"),
-    (0.75, "lightgreen"),
-    (1.0, "green"),
-]
+    color_continuous_scale = [
+        (0.0, "red"),
+        (0.25, "orange"),
+        (0.5, "yellow"),
+        (0.75, "lightgreen"),
+        (1.0, "green"),
+    ]
 
-# Crear el mapa
-mapa_fig = px.choropleth_mapbox(
-    resultados_filtrados,
-    geojson=geojson_merged,
-    locations='circuitomapa',
-    featureidkey="properties.circuitomapa",
-    color=color_column,
-    color_continuous_scale=color_continuous_scale,
-    range_color=(resultados_filtrados[color_column].min(), resultados_filtrados[color_column].max()),
-    labels={color_column: legend_name},
-    title=f"{legend_name} - {', '.join(comunas_seleccionadas)}",
-    mapbox_style="open-street-map",
-    center={"lat": -34.6118, "lon": -58.3773},
-    zoom=12,
-    opacity=0.6
-)
+    # Crear el mapa
+    mapa_fig = px.choropleth_mapbox(
+        resultados_filtrados,
+        geojson=geojson_merged,
+        locations='circuitomapa',
+        featureidkey="properties.circuitomapa",
+        color=color_column,
+        color_continuous_scale=color_continuous_scale,
+        range_color=(resultados_filtrados[color_column].min(), resultados_filtrados[color_column].max()),
+        labels={color_column: legend_name},
+        title=f"{legend_name} - {', '.join(comunas_seleccionadas)}",
+        mapbox_style="open-street-map",
+        center={"lat": -34.6118, "lon": -58.3773},
+        zoom=12,
+        opacity=0.6
+    )
 
-mapa_fig.update_coloraxes(showscale=False)
+    mapa_fig.update_coloraxes(showscale=False)
+    mapa_fig.update_geos(fitbounds="locations", visible=False)
+    mapa_fig.update_layout(
+        mapbox_zoom=12,
+        mapbox_center={"lat": -34.6118, "lon": -58.3773},
+        margin={"r": 0, "t": 0, "l": 0, "b": 0},
+        uirevision='constant'
+    )
 
-mapa_fig.update_geos(fitbounds="locations", visible=False)
-mapa_fig.update_layout(
-    mapbox_zoom=12,
-    mapbox_center={"lat": -34.6118, "lon": -58.3773},
-    margin={"r": 0, "t": 0, "l": 0, "b": 0},
-    uirevision='constant'
-)
+    # Mostrar el mapa en Streamlit
+    st.plotly_chart(mapa_fig)
 
-# Mostrar el mapa en Streamlit
-st.plotly_chart(mapa_fig)
+    # Gráfico de votos totales
+    grafico_votos_totales_fig = px.bar(
+        resultados_filtrados.sort_values(by='totvotos', ascending=False).head(20),
+        x='Circuito-Comuna',
+        y='totvotos',
+        title='Votos Totales por Circuito',
+        color_discrete_sequence=['#4B0082'],
+        template='plotly_white'
+    )
+    st.plotly_chart(grafico_votos_totales_fig)
 
-# Gráfico de votos totales
-grafico_votos_totales_fig = px.bar(
-    resultados_filtrados.sort_values(by='totvotos', ascending=False).head(20),
-    x='Circuito-Comuna',
-    y='totvotos',
-    title='Votos Totales por Circuito',
-    color_discrete_sequence=['#4B0082'],
-    template='plotly_white'
-)
-st.plotly_chart(grafico_votos_totales_fig)
+    # Gráfico de votos LLA
+    grafico_votos_lla_fig = px.bar(
+        resultados_filtrados.sort_values(by='llavotos', ascending=False).head(20),
+        x='Circuito-Comuna',
+        y='llavotos',
+        title='Votos LLA por Circuito',
+        color_discrete_sequence=['#4B0082'],
+        template='plotly_white'
+    )
+    st.plotly_chart(grafico_votos_lla_fig)
 
-# Gráfico de votos LLA
-grafico_votos_lla_fig = px.bar(
-    resultados_filtrados.sort_values(by='llavotos', ascending=False).head(20),
-    x='Circuito-Comuna',
-    y='llavotos',
-    title='Votos LLA por Circuito',
-    color_discrete_sequence=['#4B0082'],
-    template='plotly_white'
-)
-st.plotly_chart(grafico_votos_lla_fig)
+    # Gráfico de porcentaje LLA
+    grafico_porcentaje_lla_fig = px.bar(
+        resultados_filtrados.sort_values(by='%LLA', ascending=False).head(20),
+        x='Circuito-Comuna',
+        y='%LLA',
+        title='Porcentaje de Votos LLA por Circuito',
+        color_discrete_sequence=['#4B0082'],
+        template='plotly_white'
+    )
+    st.plotly_chart(grafico_porcentaje_lla_fig)
 
-# Gráfico de porcentaje LLA
-grafico_porcentaje_lla_fig = px.bar(
-    resultados_filtrados.sort_values(by='%LLA', ascending=False).head(20),
-    x='Circuito-Comuna',
-    y='%LLA',
-    title='Porcentaje de Votos LLA por Circuito',
-    color_discrete_sequence=['#4B0082'],
-    template='plotly_white'
-)
-st.plotly_chart(grafico_porcentaje_lla_fig)
+    # Gráfico de líneas
+    grafico_lineas_fig = go.Figure()
 
-# Gráfico de líneas
-grafico_lineas_fig = go.Figure()
+    grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%LLA'],
+                                            mode='lines+markers', name='%LLA', line=dict(color='violet')))
+    grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%JXC'],
+                                            mode='lines+markers', name='%JXC', line=dict(color='yellow')))
+    grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%UXP'],
+                                            mode='lines+markers', name='%UXP', line=dict(color='blue')))
 
-grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%LLA'],
-                                        mode='lines+markers', name='%LLA', line=dict(color='violet')))
-grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%JXC'],
-                                        mode='lines+markers', name='%JXC', line=dict(color='yellow')))
-grafico_lineas_fig.add_trace(go.Scatter(x=resultados_filtrados['Circuito-Comuna'], y=resultados_filtrados['%UXP'],
-                                        mode='lines+markers', name='%UXP', line=dict(color='blue')))
+    grafico_lineas_fig.add_hline(y=resultados_filtrados['%LLA'].mean(), line_dash="dash", annotation_text="Promedio LLA", line_color="violet")
+    grafico_lineas_fig.add_hline(y=resultados_filtrados['%JXC'].mean(), line_dash="dash", annotation_text="Promedio JXC", line_color="yellow")
+    grafico_lineas_fig.add_hline(y=resultados_filtrados['%UXP'].mean(), line_dash="dash", annotation_text="Promedio UXP", line_color="blue")
 
-grafico_lineas_fig.add_hline(y=resultados_filtrados['%LLA'].mean(), line_dash="dash", annotation_text="Promedio LLA", line_color="violet")
-grafico_lineas_fig.add_hline(y=resultados_filtrados['%JXC'].mean(), line_dash="dash", annotation_text="Promedio JXC", line_color="yellow")
-grafico_lineas_fig.add_hline(y=resultados_filtrados['%UXP'].mean(), line_dash="dash", annotation_text="Promedio UXP", line_color="blue")
-
-grafico_lineas_fig.update_layout(title="Porcentajes de Votos por Partido", xaxis_title="Circuito", yaxis_title="Porcentaje")
-st.plotly_chart(grafico_lineas_fig)
+    grafico_lineas_fig.update_layout(title="Porcentajes de Votos por Partido", xaxis_title="Circuito", yaxis_title="Porcentaje")
+    st.plotly_chart(grafico_lineas_fig)
 
 elif tabs == "Comunas":
-st.subheader("Comunas")
+    st.subheader("Comunas")
 
-tipo_visualizacion = st.selectbox("Selecciona el tipo de visualización:", ['Votos', 'Porcentaje'])
+    tipo_visualizacion = st.selectbox("Selecciona el tipo de visualización:", ['Votos', 'Porcentaje'])
 
     # Función para crear los gráficos según la selección
-def crear_graficos(tipo_visualizacion):
+    def crear_graficos(tipo_visualizacion):
         if tipo_visualizacion == 'Votos':
             color_column = 'llaxvoto'
             legend_name = 'Votos de LLA por Comuna'
@@ -527,8 +528,8 @@ def crear_graficos(tipo_visualizacion):
         return mapa_fig_comunas, grafico_votos_comuna_fig
 
     # Crear gráficos solo después de seleccionar la opción
-mapa_fig, grafico_votos_comuna_fig = crear_graficos(tipo_visualizacion)
+    mapa_fig, grafico_votos_comuna_fig = crear_graficos(tipo_visualizacion)
 
     # Mostrar gráficos
-st.plotly_chart(mapa_fig)
-st.plotly_chart(grafico_votos_comuna_fig)
+    st.plotly_chart(mapa_fig)
+    st.plotly_chart(grafico_votos_comuna_fig)
